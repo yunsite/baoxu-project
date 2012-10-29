cc487a12b15175bde15837c5126ce76b63cfedf2
chbaoxu <chbaoxu@edc16927-10bf-e0ca-af63-d3ecab1c231c>
2012-10-29 07:32:22 +0000

    新闻ICS版，左边导航栏架构完成

---
+++ 05-baoxu-news-ics/scripts/function.js
@@ -0,0 +1,28 @@
+/**
+ * Author: Baoxu
+ * Date:   12-10-29
+ * Time:   下午12:09
+ */
+
+addLoadEvent(test);
+
+function test(){
+	init();
+}
+
+function init(){
+
+	var client_width = getClientInfo()["width"];
+	var client_height = getClientInfo()["height"];
+
+	var bot_layer = document.getElementById("bot-layer");
+	bot_layer.style.width = client_width;
+	bot_layer.style.height = client_height;
+
+	document.getElementById("bot-layer-navi").style.height = client_height + "px";
+	document.getElementById("bot-layer-navi-shadow").style.height = client_height + "px";
+	document.getElementById("bot-layer-info-shadow").style.height = client_height + "px";
+	document.getElementById("bot-layer-info").style.height = client_height + "px";
+
+	document.getElementById("bot-layer-info").style.width = client_width - 96 + "px";
+}
