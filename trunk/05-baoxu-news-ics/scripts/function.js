04d29c70d0cb786f1451a989e1bccfbf24d09e15
chbaoxu <chbaoxu@edc16927-10bf-e0ca-af63-d3ecab1c231c>
2012-10-30 06:20:29 +0000

    完成底层导航的可点击切换效果

--- trunk/05-baoxu-news-ics/scripts/function.js
+++ trunk/05-baoxu-news-ics/scripts/function.js
@@ -4,27 +4,46 @@
  * Time:   下午12:09
  */
 
-addLoadEvent(test);
+addLoadEvent(todo);
 
-function test(){
-	init();
+function todo(){
+	initViewPort();
+	changeBotLayerNavi();
 }
 
-function init(){
-
+//初始化页面的高宽以适合所有屏幕的显示
+function initViewPort(){
+	//获取屏幕信息
 	var client_width = getClientInfo()["width"];
 	var client_height = getClientInfo()["height"];
 
 	console.log("页面宽高：" + client_width + ":" + client_height);
+	//alert("页面宽高：" + client_width + ":" + client_height);
 
-	var bot_layer = document.getElementById("bot-layer");
-	bot_layer.style.width = client_width;
-	bot_layer.style.height = client_height;
-
+	//页面底层图层全比例显示
+	document.getElementById("bot-layer").style.width = client_width;
+	document.getElementById("bot-layer").style.height = client_height;
+	//底层导航，导航遮罩阴影，底层个人信息，个人信息遮罩阴影校正高度
 	document.getElementById("bot-layer-navi").style.height = client_height + "px";
 	document.getElementById("bot-layer-navi-shadow").style.height = client_height + "px";
 	document.getElementById("bot-layer-info-shadow").style.height = client_height + "px";
 	document.getElementById("bot-layer-info").style.height = client_height + "px";
-
+	//底层个人信息部分校正宽度
 	document.getElementById("bot-layer-info").style.width = client_width - 96 + "px";
+}
+
+//页面底层导航按钮点击效果
+function changeBotLayerNavi(){
+	var bot_layer_navi_li = document.getElementById("navi-list").getElementsByTagName("li");
+	console.log("li的个数:" + bot_layer_navi_li.length);
+	for(var i = 0; i<bot_layer_navi_li.length; i++){
+		bot_layer_navi_li[i].getElementsByTagName("a")[0].onclick = function(){
+			//某个按钮被点击后，将所有的按钮样式置空
+			for(var t = 0; t<bot_layer_navi_li.length; t++){
+				bot_layer_navi_li[t].getElementsByTagName("a")[0].className = "";
+			}
+			//将被点击按钮的样式置为current
+			this.className = "current";
+		}
+	}
 }
