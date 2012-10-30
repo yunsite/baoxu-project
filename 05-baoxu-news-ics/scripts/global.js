022afdda8e231d04d56def1f9f5f1dfc8b1aef8e
chbaoxu <chbaoxu@gmail.com>
2012-10-30 15:02:06 +0000

    初步完成左右抽屉的效果

--- 05-baoxu-news-ics/scripts/global.js
+++ 05-baoxu-news-ics/scripts/global.js
@@ -96,21 +96,19 @@ wheelEvent.prototype.wheel = function(event){
 
 
 /**
- * 此方法用于移动元素到指定坐标，可以自定义沿X轴或Y轴移动，自定义单步移动距离百分比和单步移动时间
+ * 此方法用于移动元素到指定坐标，自定义单步移动距离百分比和单步移动时间
  *
  * @parameter elementID：需要移动的块的ID
- * @parameter moveType：移动的类型，x表示横向移动，y表示纵向移动，xy表示同时横纵向移动
  * @parameter targetX：移动到的目标X轴坐标
- * @parameter targetY：移动到的目标Y轴坐标
  * @parameter stepDis：每一次timeOut的移动距离百分比，用小数表示
  * @parameter stepTime：每一次timeOut的时间
+ * @parameter key:用于标识关联影响的元素是应该变宽还是变窄，取值为-1，0，1
  *
  * @return true:表示移动完成
  * */
-function moveElementTo(elementID, moveType, targetX, targetY, stepDis, stepTime){
+function moveElementTo(elementID, targetX, stepDis, stepTime , key){
 	var elementToMove = document.getElementById(elementID);
 	var elementX = parseInt(elementToMove.style.marginLeft);
-	var elementY = parseInt(elementToMove.style.marginTop);
 	var dist;
 
 	//如果上一次的点击的移位还没有完成，先清除循环
@@ -118,116 +116,40 @@ function moveElementTo(elementID, moveType, targetX, targetY, stepDis, stepTime)
 		clearTimeout(elementToMove.movement);
 	}
 
-	//如果移动类型参数是x则做横向移动
-	if(moveType == "x"){
-		//移位完成，返回true
-		if(elementX == targetX){
-			return true;
-		}
-
-		if(elementX > targetX){
-			dist = Math.ceil((elementX - targetX) * stepDis);
-			elementX -= dist;
-		}
-
-		if(elementX < targetX){
-			dist = Math.ceil((targetX - elementX) * stepDis);
-			elementX += dist;
-		}
-		//设定单次移动之后的marginLeft值
-		elementToMove.style.marginLeft = elementX + "px";
+	//移位完成，返回true
+	if(elementX == targetX){
+		return true;
 	}
-	//移动类型是y做纵向移动
-	if(moveType == "y"){
-		//移位完成，返回true
-		if(elementY == targetY){
-			return true;
-		}
 
-		if(elementY > targetY){
-			dist = Math.ceil((elementY - targetY) * stepDis);
-			elementY -= dist;
-		}
+	if(elementX > targetX){
+		dist = Math.ceil((elementX - targetX) * stepDis);
+		elementX -= dist;
+	}
 
-		if(elementY < targetY){
-			dist = Math.ceil((targetY - elementY) * stepDis);
-			elementY += dist;
-		}
-		//设定单次移动之后的marginTop值
-		elementToMove.style.marginTop = elementY + "px";
+	if(elementX < targetX){
+		dist = Math.ceil((targetX - elementX) * stepDis);
+		elementX += dist;
 	}
-	//移动类型是xy做对角线移动
-	if(moveType == "xy"){
-		//移位完成，返回true
-		if(elementX == targetX && elementY == targetY){
-			return true;
-		}
-		if(elementX > targetX){
-			dist = Math.ceil((elementX - targetX) * stepDis);
-			elementX -= dist;
-		}
-		if(elementY > targetY){
-			dist = Math.ceil((elementY - targetY) * stepDis);
-			elementY -= dist;
-		}
-		if(elementX < targetX){
-			dist = Math.ceil((targetX - elementX) * stepDis);
-			elementX += dist;
-		}
-		if(elementY < targetY){
-			dist = Math.ceil((targetY - elementY) * stepDis);
-			elementY += dist;
-		}
-		//设定单次移动之后的marginLeft值
-		elementToMove.style.marginLeft = elementX + "px";
-		//设定单次移动之后的marginTop值
-		elementToMove.style.marginTop = elementY + "px";
-	}
+	//设定单次移动之后的marginLeft值
+	elementToMove.style.marginLeft = elementX + "px";
+
+	elementToMove.style.width = key*dist + parseInt(elementToMove.style.width) + "px";
 
 	//循环单次移动，形成动画效果
-	var repeat = "moveElementTo('" + elementID + "','" + moveType + "'," + targetX + "," + targetY + "," + stepDis + "," + stepTime + ")";
+	var repeat = "moveElementTo('" + elementID  + "'," + targetX + "," + stepDis + "," + stepTime + "," + key+ ")";
 	elementToMove.movement = setTimeout(repeat, stepTime);
 }
 
 
-/**
- * 此方法用于移动元素到指定坐标，可以自定义沿X轴或Y轴移动，自定义单步移动距离百分比和单步移动时间
- *
- * @parameter elementID：需要移动的块的ID
- * @parameter moveType：移动的类型，x表示横向移动，y表示纵向移动，xy表示同时横纵向移动
- * @parameter stepX：此次触发要移动的X轴距离
- * @parameter stepY：此次触发要移动的Y轴距离
- * @parameter stepDis：每一次timeOut的移动距离百分比，用小数表示
- * @parameter stepTime：每一次timeOut的时间
- *
- * @return true:表示移动完成
- * */
-function moveElementWith(elementID, moveType, stepX, stepY, stepDis, stepTime){
+function moveElementWith(elementID, stepX, stepDis, stepTime, key){
 	var elementToMove = document.getElementById(elementID);
 	//获取当前X轴坐标
 	var elementX = parseInt(elementToMove.style.marginLeft);
-	//获取当前Y轴坐标
-	var elementY = parseInt(elementToMove.style.marginTop);
-	//获取这个元素的高度值
-	var elementH = parseInt(elementToMove.scrollHeight) - 620;
-	//alert(elementH);
+	//alert(elementToMove.style.marginLeft);
 
 	var targetX = elementX + stepX;
-	var targetY = elementY + stepY;
-
-	//如果距离顶部为0但是用户还在往下滑，则弹回去0
-	if(elementY > -stepY && stepY > 0){
-		var repeat = "moveElementTo('" + elementID + "','" + moveType + "'," + targetX + "," + 0 + "," + stepDis + "," + 50 + ")";
-		elementToMove.movement = setTimeout(repeat, stepTime);
-	}
-
-	//如果距离底部为0但是用户还在往上滑，则弹回去
-	if(elementY < -elementH && stepY < 0){
-		var repeat = "moveElementTo('" + elementID + "','" + moveType + "'," + targetX + "," + -elementH + "," + stepDis + "," + 50 + ")";
-		elementToMove.movement = setTimeout(repeat, stepTime);
-	}
 
 	//循环单次移动，形成动画效果
-	var repeat = "moveElementTo('" + elementID + "','" + moveType + "'," + targetX + "," + targetY + "," + stepDis + "," + stepTime + ")";
+	var repeat = "moveElementTo('" + elementID + "'," + targetX + "," + stepDis + "," + stepTime + "," + key+ ")";
 	elementToMove.movement = setTimeout(repeat, stepTime);
 }
