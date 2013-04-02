faea93340caf3fe87085d4c1c435464c2b762f2d
chbaoxu@gmail.com <chbaoxu@gmail.com@edc16927-10bf-e0ca-af63-d3ecab1c231c>
2013-04-02 15:51:15 +0000

    加密COOKIE，验证COOKIE的有效性，优化登录与报头

--- trunk/08-e-library/common/conn.php
+++ trunk/08-e-library/common/conn.php
@@ -7,7 +7,7 @@
 
 
 
-$host = "127.0.0.1";
+$host = "192.168.1.14";
 $user = "baoxu";
 $pass = ",.";
 $db = "library";
--- trunk/08-e-library/common/nav.php
+++ trunk/08-e-library/common/nav.php
@@ -1,7 +1,7 @@
 <?php
+header("Content-Type:text/html; charset=utf-8");
 include "conn.php";
-
-
+include "function.php";
 ?>
 
 <!--顶部导航-->
@@ -21,36 +21,41 @@ include "conn.php";
                     <li class = "active"><a href = "#">首页</a></li>
                     <li><a href = "../book/">书目列表</a></li>
                     <li><a href = "../user/">个人中心</a></li>
-                    <li class = "dropdown f-dn">
-                        <a data-toggle = "dropdown" class = "dropdown-toggle" href = "#">Dropdown <b
+                    <li class = "dropdown">
+                        <a data-toggle = "dropdown" class = "dropdown-toggle" href = "#">管理后台 <b
                                 class = "caret"></b></a>
                         <ul class = "dropdown-menu">
-                            <li><a href = "#">Action</a></li>
-                            <li><a href = "#">Another action</a></li>
-                            <li><a href = "#">Something else here</a></li>
+                            <li class = "nav-header">书籍管理</li>
+                            <li><a href = "#">添加书籍</a></li>
+                            <li><a href = "#">书籍列表</a></li>
                             <li class = "divider"></li>
-                            <li class = "nav-header">Nav header</li>
-                            <li><a href = "#">Separated link</a></li>
-                            <li><a href = "#">One more separated link</a></li>
+                            <li class = "nav-header">用户管理</li>
+                            <li><a href = "#">添加用户</a></li>
+                            <li><a href = "#">用户列表</a></li>
                         </ul>
                     </li>
                 </ul>
                 <!--判断cookie-->
                 <?php
-                if (isset($_COOKIE["userId"])) {
+                if (isset($_COOKIE["userCode"])) {
+                    if($_COOKIE["userName"]){
+                        $displayName = $_COOKIE["userName"];
+                    }else{
+                        $displayName = substr($_COOKIE["userMail"],0,strpos($_COOKIE["userMail"],"@"));
+                    }
                     echo '<form class = "navbar-form pull-right f-dn" id = "loginForm" name = "loginForm" action = "" onsubmit = "return false;">
                     <input type = "text" placeholder = "邮箱前缀" class = "span2" name = "email" required="required"  id = "email">
                     <input type = "password" placeholder = "密码" class = "span2" name = "password" required="required"  id = "password">
                     <button class = "btn" id = "bx-login-btn" onclick = "doLogin()">登录</button>
                 </form>
-                <div id="loginSuccess" class = "navbar-form pull-right bx-login-success">陈保需 <a href="#" onclick="doLogout()"><span>注销</span></a></div>';
+                <div id="loginSuccess" class = "navbar-form pull-right bx-login-success">'.$displayName.' <a href="#" onclick="doLogout()"><span>注销</span></a></div>';
                 } else {
                     echo '<form class = "navbar-form pull-right" id = "loginForm" name = "loginForm" action = "" onsubmit = "return false;">
                     <input type = "text" placeholder = "邮箱前缀" class = "span2" name = "email" required="required"  id = "email">
                     <input type = "password" placeholder = "密码" class = "span2" name = "password" id = "password">
                     <button class = "btn" id = "bx-login-btn" onclick = "doLogin()">登录</button>
                 </form>
-                <div id = "loginSuccess" class = "navbar-form pull-right bx-login-success f-dn">陈保需 <a href = "#" onclick="doLogout()"><span>注销</span></a></div>';
+                <div id = "loginSuccess" class = "navbar-form pull-right bx-login-success f-dn"><span id="displayUserName"></span>&nbsp;<a href = "#" onclick="doLogout()"><span>注销</span></a></div>';
                 }
                 ?>
 
