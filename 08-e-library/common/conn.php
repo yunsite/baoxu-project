ee92cec0432593df72ee8771d8de7b3b0d6658a4
chbaoxu <chbaoxu@gmail.com>
2013-04-07 10:12:43 +0000

    重构Global文件

--- 08-e-library/common/conn.php
+++ 08-e-library/common/conn.php
@@ -6,13 +6,8 @@
  * 连接数据库
  */
 
-$host = "192.168.1.14";
-$user = "baoxu";
-$pass = ",.";
-$db = "library";
-
-$conn = mysql_connect($host, $user, $pass);
-mysql_select_db($db, $conn);
-mysql_query("SET NAMES 'utf8'");
-
 include "global.php";
+
+$conn = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+mysql_select_db(DB_NAME, $conn);
+mysql_query("SET NAMES 'utf8'");
--- 08-e-library/common/global.php
+++ 08-e-library/common/global.php
@@ -11,5 +11,14 @@ define("HOST", "localhost");
 //项目根目录
 define("DOCROOT", "/baoxu-project/08-e-library/");
 
+//数据库地址
+define("DB_HOST", "127.0.0.1");
+//数据库用户名
+define("DB_USER", "baoxu");
+//数据库密码
+define("DB_PASS", ",.");
+//数据库名
+define("DB_NAME", "library");
+
 //用户可借阅的天数
 define("BORROW_DAY", 30);
